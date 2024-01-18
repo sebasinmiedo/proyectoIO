e45b5797213651a6cb081cab4c631e840942fe8c
sebasinmiedo <sebastianalbertomaqueraochoa@gmail.com>
2024-01-18 02:22:49 -0500

    Mejora del diseno del menu de opciones

--- menu.py
+++ menu.py
@@ -4,6 +4,7 @@ from CPM import *
 from PERT import *
 from DT import *
 from EOQ import *
+from TColas import *
 
 # Funciones para cada modelo
 def abrir_ventana_cpm():
@@ -13,10 +14,22 @@ def abrir_ventana_pert():
     ventana_pert()
 
 def abrir_ventana_eoq():
-    ventana_inventario();
+    ventana_inventario()
 
 def abrir_ventana_decisiones():
     ventana_DT()
+
+def abrir_ventana_tcolas():
+    ventana_tcolas()
+
+# Nuevas funciones para los botones adicionales
+def abrir_ventana_simulacion():
+    # Agrega aquí la lógica para la simulación de MonteCarlo
+    pass
+
+def abrir_ventana_programacion_cuadratica():
+    # Agrega aquí la lógica para la programación cuadrática
+    pass
 
 # Función para salir del programa
 def salir():
@@ -27,12 +40,21 @@ def salir():
 # Configuración de la ventana principal
 ventana = tk.Tk()
 ventana.title("Menú de Modelos de Investigación Operativa II")
-ventana.geometry("500x400")
+ventana.geometry("600x700")
+ventana.configure(bg='#E0E0E0')  # Cambia el color de fondo de la ventana
 
 # Estilo para los botones
-estilo_boton = {"font": ("Arial", 14), "width": 20, "height": 2}
+estilo_boton = {
+    "font": ("Arial", 14),
+    "width": 30,
+    "height": 2,
+    "bg": '#4CAF50',  # Cambia el color de fondo de los botones
+    "fg": 'white',    # Cambia el color del texto de los botones
+    "borderwidth": 2,
+    "relief": 'raised'
+}
 
-# Botones para los modelos
+# Botones para los modelos existentes
 boton_cpm = tk.Button(ventana, text="CPM", command=abrir_ventana_cpm, **estilo_boton)
 boton_cpm.pack(pady=10)
 
@@ -44,6 +66,16 @@ boton_eoq.pack(pady=10)
 
 boton_decisiones = tk.Button(ventana, text="Teoría de Decisiones", command=abrir_ventana_decisiones, **estilo_boton)
 boton_decisiones.pack(pady=10)
+
+boton_tcolas = tk.Button(ventana, text="Teoría de Colas", command=abrir_ventana_tcolas, **estilo_boton)
+boton_tcolas.pack(pady=10)
+
+# Nuevos botones
+boton_simulacion = tk.Button(ventana, text="Simulación de MonteCarlo", command=abrir_ventana_simulacion, **estilo_boton)
+boton_simulacion.pack(pady=10)
+
+boton_programacion_cuadratica = tk.Button(ventana, text="Programación Cuadrática", command=abrir_ventana_programacion_cuadratica, **estilo_boton)
+boton_programacion_cuadratica.pack(pady=10)
 
 # Botón para salir del programa
 boton_salir = tk.Button(ventana, text="Salir", command=salir, **estilo_boton)
